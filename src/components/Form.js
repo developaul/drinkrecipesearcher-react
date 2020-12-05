744e3b44be959a837c92df02fdf693f663fe48c5
Paul Chávez <paulchavezromero20@gmail.com>
2020-12-05 18:43:21 -0500

    estilos del formulario added

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,9 +2,39 @@ import React from 'react';
 
 const Form = () => {
     return (
-        <div>
-            Formulario
-        </div>
+        <form className="col-12">
+            <fieldset className="text-center">
+                <legend>Busca Bebidas por Categoría o Ingrediente</legend>
+            </fieldset>
+
+            <div className="row mt-4">
+                <div className="col-md-4">
+                    <input
+                        placeholder="Buscar por Ingrediente"
+                        className="form-control"
+                        name="nombre"
+                        type="text"
+                    />
+                </div>
+
+                <div className="col-md-4">
+                    <select
+                        className="form-control"
+                        name="categoria"
+                    >
+                        <option value="">-- Seleciona Categoría --</option>
+                    </select>
+                </div>
+
+                <div className="col-md-4">
+                    <input
+                        className="btn btn-block btn-primary"
+                        type="submit"
+                        value="Buscar Bebidas"
+                    />
+                </div>
+            </div>
+        </form>
     );
 };
 
